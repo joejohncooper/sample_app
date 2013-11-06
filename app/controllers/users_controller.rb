851c717afad867993cb6eaacd348d11ddeb7f528
Joe Cooper <joejohncooper@gmail.com>
2013-11-06 19:42:59 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ before_action :admin_user,    only: :destroy
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def destroy
@@ -54,12 +55,12 @@ before_action :admin_user,    only: :destroy
 
 	#before filters
 
-	def signed_in_user
-		unless signed_in?
-			store_location
-			redirect_to signin_url, notice: "Please sign in."
-		end
-	end
+	# def signed_in_user
+	# 	unless signed_in?
+	# 		store_location
+	# 		redirect_to signin_url, notice: "Please sign in."
+	# 	end
+	# end
 
     def correct_user
       @user = User.find(params[:id])
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,9 +22,16 @@ module SessionsHelper
 		user == current_user
 	end
 
-	  def current_user=(user)
-    @current_user = user
-  end
+	def current_user=(user)
+    	@current_user = user
+    end
+
+    def signed_in_user
+    	unless signed_in?
+    		store_location
+    		redirect_to signin_url, notice: "Please sign in."
+    	end
+    end
 
 	def sign_out
 		self.current_user = nil
