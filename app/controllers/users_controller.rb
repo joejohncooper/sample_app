08509726f630a7bdcec82fd05cc922239feadc32
Joe Cooper <joejohncooper@gmail.com>
2013-11-12 19:54:58 +0000

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :signed_in_user, only: [:index, :edit, :update]
+before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
 before_action :correct_user,	only: [:edit, :update]
 before_action :admin_user,    only: :destroy
 
@@ -44,8 +44,22 @@ before_action :admin_user,    only: :destroy
   			redirect_to @user
   		else
   			render 'edit'
-  		end
   	end
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers 
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
 	private
 
